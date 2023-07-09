a3ebf4855cb62b81c7e6418cb1c25b9b5144cb8d
RAJ K. RANA <rajkumarrana594@gmail.com>
2023-07-09 15:06:59 +0530

    first commit

---
+++ JS B-1/Session - 01/Index.js
@@ -0,0 +1,21 @@
+// Print A Hello World
+
+// How to print in JS ???
+
+// Console.log  -> Takes a msg and print it
+
+console.log("Hello Wolrd");
+console.log("My Name is Raj");
+console.log(100);
+console.log(100.11);
+console.log(true);
+console.log(false);
+console.log(null);
+console.log(undefined);
+
+// type of variables -> returns the data type of value
+
+console.log(typeof "Hello World");
+console.log(typeof 100);
+console.log(typeof true);
+console.log(typeof undefined);
